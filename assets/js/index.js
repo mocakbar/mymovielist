9c927b5da3f594c622697470f9e320c6f9e89169
brindel <akarmoc@gmail.com>
2020-06-22 20:49:00 +0700

    full pack news, upcoming movies dengan dark theme done

--- assets/js/index.js
+++ assets/js/index.js
@@ -278,6 +278,8 @@ let a  = "false"
 			}
 	})
 
+
+	// membuat tombol function to the top
 	$(document).on('scroll', function(){
 		scrollTopFunction();
 	})
@@ -294,8 +296,35 @@ let a  = "false"
 		$(window).scrollTop(0)
 	})
 
-	$.getJSON('news.json', function(data){
-		console.log(data)
+	// membuat tombol call funtion API Upcoming.
+	$.ajax({
+		url: "https://api.themoviedb.org/3/movie/upcoming?&page=1",
+			data: { "api_key": "4dda69557f9d1b4d4930dd9ee950047d" },
+			dataType: "json",
+			success: function(result, status, xhr){
+				let resultHtml = '';
+				
+				for(i=0; i<result['results'].length; i++){
+
+					let img = "https://image.tmdb.org/t/p/w500/" + result['results'][i]['poster_path'];
+
+					let title = result['results'][i]['title']
+					let release = result['results'][i]['release_date']
+
+					resultHtml += '<div class="upcoming-movie">'
+					resultHtml += "<img src='" + img + "'>"
+					resultHtml += "<h4>" + title + "</h4>"
+					resultHtml += "<p>Release : " + release + "</p>"
+					resultHtml += "</div>"
+
+				}
+
+				$(".upcoming .main-body").html(resultHtml)
+
+				console.log(result)
+			}
 	})
+
+
 })
 
